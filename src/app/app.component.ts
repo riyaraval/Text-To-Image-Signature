487ac0a331f424a43dacf3653fe788189d3f57d6
riyaraval1512 <riya.raval@bacancytechnology.com>
2020-04-17 19:55:56 +0530

    selected Signature Download Done

---
+++ src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+import * as html2canvas from 'html2canvas';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'TextToSignature';
+  // screen;
+
+  @Input() fullname:string ='Riya Raval';
+  @Input() initial:string = 'RR';
+
+  selectedScreen:ElementRef;
+  @ViewChild('screen1',{static:false}) screen1: ElementRef;
+  @ViewChild('screen2',{static:false}) screen2: ElementRef;
+  @ViewChild('screen3',{static:false}) screen3: ElementRef;
+  @ViewChild('canvas',{static:false}) canvas: ElementRef;
+  @ViewChild('downloadLink',{static:false}) downloadLink: ElementRef;
+  
+  createSignature(screen){
+    console.log(screen)
+    if(screen=='screen1'){
+      this.selectedScreen=this.screen1;
+      html2canvas(this.screen1.nativeElement).then(canvas => {
+        this.canvas.nativeElement.src = canvas.toDataURL();
+      });
+    }
+
+    if(screen=='screen2'){
+      this.selectedScreen=this.screen2;
+      html2canvas(this.screen2.nativeElement).then(canvas => {
+        this.canvas.nativeElement.src = canvas.toDataURL();
+      });
+    }
+
+    if(screen=='screen3'){
+      this.selectedScreen=this.screen3;
+      html2canvas(this.screen3.nativeElement).then(canvas => {
+        this.canvas.nativeElement.src = canvas.toDataURL();
+      });
+    }
+  }
+
+  createLink(){
+    html2canvas(this.selectedScreen.nativeElement).then(canvas => {
+      // this.canvas.nativeElement.src = canvas.toDataURL();
+      this.downloadLink.nativeElement.href = canvas.toDataURL('image/png;base64');
+      console.log(canvas.toDataURL());
+      this.downloadLink.nativeElement.download = 'Signature.png';
+      this.downloadLink.nativeElement.click();
+    });
+  }
+}
